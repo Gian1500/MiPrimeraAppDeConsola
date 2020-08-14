69bcfe36fed8e3a81d310b2068f8507de8ccf4bd
Gianfranco Curci <gianuniversidad@gmail.com>
2020-08-13 21:58:03 -0300

    Primer Commit

---
+++ MiPrimeraAppDeConsola/MiPrimeraAppDeConsola/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimeraAppDeConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo :D !");
+            Console.Write("Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
